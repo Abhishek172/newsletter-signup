837df6c0090a050902eac7a23be69d2d0aec134d
Abhishek Rai <champs172@gmail.com>
2021-10-07 21:23:44 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const request = require('request');
+const bodyParser = require('body-parser');
+const https = require('https');
+const { Http2ServerResponse } = require('http2');
+
+const app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname+"/signup.html");
+});
+
+app.post("/", function(req, res){
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const email = req.body.email;
+
+    const data = {
+        members: [{
+                email_address:email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }]
+    }
+
+    const jsonData = JSON.stringify(data);
+    const apiId = 'your api id';
+    const audienceId = 'your audience id';
+    const url = "https://us5.api.mailchimp.com/3.0/lists/"+audienceId+"";
+
+    const options = {
+        method:"POST",
+        auth: "foxtrot:"+apiId+""
+    }
+
+    const request = https.request(url, options, function(response){
+
+        if(response.statusCode === 200){
+            res.sendFile(__dirname+"/success.html");
+        } else {
+            res.sendFile(__dirname+"/failure.html");
+        }
+
+        response.on("data", function(data){
+            console.log(JSON.parse(data))
+        })
+    });
+
+    request.write(jsonData);
+    request.end();
+
+});
+
+app.post("/failure", function(req, res){
+    res.redirect("/");
+})
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Server running on port 3000");
+});
